e5cbd2dba2e46a8acd8a4be0bc5f532801a82fe7
appleEmily <emiri3128@gmail.com>
2019-06-14 22:57:22 +0900

    Initial Commit

---
+++ chikichiki count/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  chikichiki count
+//
+//  Created by 野崎絵未里 on 2019/04/27.
+//  Copyright © 2019年 野崎絵未里. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var number:Int = 0
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+   
+
+
+}
+
