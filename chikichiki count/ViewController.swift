13d6684f503bef0af0f27d95f323ff236c3f1841
appleEmily <emiri3128@gmail.com>
2019-06-15 23:26:20 +0900

    practice

--- chikichiki count/ViewController.swift
+++ chikichiki count/ViewController.swift
@@ -9,20 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var number:Int = 0
-    
-    @IBOutlet var label:UILabel!
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-    
-    @IBAction func plus() {
-        number += 1
-        label.text = String(number)
-    }
+
 
 
 }
