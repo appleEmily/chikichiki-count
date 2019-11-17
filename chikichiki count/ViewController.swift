84dd9d67d3da28164493c9656df2573f707e2132
appleEmily <emiri3128@gmail.com>
2019-11-17 16:14:27 +0900

    久しぶりchikichiki

--- chikichiki count/ViewController.swift
+++ chikichiki count/ViewController.swift
@@ -1,30 +1,46 @@
-//
-//  ViewController.swift
+ //  ViewController.swift
 //  chikichiki count
 //
 //  Created by 野崎絵未里 on 2019/04/27.
 //  Copyright © 2019年 野崎絵未里. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    var number: Int = 0
-    @IBOutlet weak var labrl: UILabel!
     
-
+    var number:Int = 0
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     @IBAction func plus(_ sender: Any) {
         number = number + 1
-        labrl.text = String(number)
+        label.text = String(number)
     }
     
-
-
+    @IBAction func minuns(_ sender: Any) {
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    @IBAction func multiple(_ sender: Any) {
+        number = number * 4
+        label.text = String(number)
+    }
+    
+    @IBAction func devide(_ sender: Any) {
+        number = number / 3
+        label.text = String(number)
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        number = 0
+        label.text = String(number)
+    }
+    
+    
 }
-
